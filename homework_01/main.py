498daaf34205bbe2de02bc95db632e07f83f3140
temirs <95910024+temirs@users.noreply.github.com>
2021-12-17 14:37:53 +0600

    Update main.py

--- homework_01/main.py
+++ homework_01/main.py
@@ -1,32 +1,39 @@
-"""
-Домашнее задание №1
-Функции и структуры данных
-"""
+import math
+
+def power_numbers(*list):
+    return [i ** 2 for i in list]
 
 
-def power_numbers():
-    """
-    функция, которая принимает N целых чисел,
-    и возвращает список квадратов этих чисел
-    >>> power_numbers(1, 2, 5, 7)
-    <<< [1, 4, 25, 49]
-    """
-
-
-# filter types
 ODD = "odd"
 EVEN = "even"
 PRIME = "prime"
 
+def is_prime(list):
+    d = []
+    for i in list:
+        j = 2
+        while j <= math.sqrt(i):
+            if i % j == 0:
+                break
+            j += 1
+        else:
+            if i != 0:
+                d.append(i)
+    return d
 
-def filter_numbers():
-    """
-    функция, которая на вход принимает список из целых чисел,
-    и возвращает только чётные/нечётные/простые числа
-    (выбор производится передачей дополнительного аргумента)
 
-    >>> filter_numbers([1, 2, 3], ODD)
-    <<< [1, 3]
-    >>> filter_numbers([2, 3, 4, 5], EVEN)
-    <<< [2, 4]
-    """
+def filter_numbers(list, func):
+
+    def ODD(list):
+        return [i for i in list if i % 2 != 0]
+
+    def EVEN(list):
+        return [i for i in list if i % 2 == 0]
+
+    if func == 'odd':
+        return ODD(list)
+    elif func == 'even':
+        return EVEN(list)
+    elif func == 'prime':
+        return is_prime(list)
+    
